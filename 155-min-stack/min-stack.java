e2cad3e7c70c4693571dfdde400eaee20a72879b
vinodgadaputi <vinodgadaputi168@gmail.com>
2026-02-04 20:40:49 +0530

    Time: 5 ms (83.11%) | Memory: 47.2 MB (23.53%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,32 @@
+class MinStack {
+    private Deque<Integer> stack;
+    private Deque<Integer> minStack;
+    
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
