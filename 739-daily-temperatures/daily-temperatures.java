4d2a8e1b44e5635402585d537769d64ab6206cfb
vinodgadaputi <vinodgadaputi168@gmail.com>
2026-02-04 20:32:22 +0530

    Time: 22 ms (94.76%) | Memory: 103.7 MB (84.34%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();  // indices stack
+        
+        for (int i = 0; i < n; i++) {
+            // Pop indices where current temp is warmer
+            while (!stack.isEmpty() && 
+                   temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+        
+        return answer;
+    }
+}
