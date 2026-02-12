69277046bfee98877fdf283bf02213354f70b2f3
vinodgadaputi <vinodgadaputi168@gmail.com>
2026-02-12 09:03:47 +0530

    Time: 21 ms (79.50%) | Memory: 59.4 MB (64.86%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -0,0 +1,15 @@
+class RecentCounter {
+    private Queue<Integer> queue;
+    
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        while (!queue.isEmpty() && queue.peek() < t - 3000) {
+            queue.poll();
+        }
+        queue.offer(t);
+        return queue.size();
+    }
+}
