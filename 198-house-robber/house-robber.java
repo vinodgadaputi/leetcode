f54d0b2426577c74f50af50cfad84c7927a48e5d
vinodgadaputi <vinodgadaputi168@gmail.com>
2026-04-29 17:44:47 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (19.11%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        int prev2 = nums[0];              // max loot up to index 0
+        int prev1 = Math.max(nums[0], nums[1]); // max loot up to index 1
+
+        for (int i = 2; i < n; i++) {
+            int curr = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
